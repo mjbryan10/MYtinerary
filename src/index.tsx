baee2969f51e1ceac7ead80fa996b3c68ff6171d
mjbryan <mjbyran10@gmail.com>
2020-02-24 15:28:38 +0100

    ub progress

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './components/app/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./components/app/App";
+import * as serviceWorker from "./serviceWorker";
 
 //REDUX
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
-const store = createStore(rootReducer);
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import rootReducer from './store/rootReducer';
+// const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+//REDUX
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers/rootReducer";
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
